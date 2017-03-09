ffb66f78cb98e5475fd19ddee22ba23d50afcaf2
TSConroe <1conroe1@gmail.com>
2017-03-09 21:18:13 +0200

    upload part audio list to text box

--- VkAudioBase/Form1.cs
+++ VkAudioBase/Form1.cs
@@ -7,14 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenQA.Selenium;
+using System.Threading;
 
 namespace VkAudioBase
 {
     public partial class Form1 : Form
     {
+        IWebDriver Brouser;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            OpenQA.Selenium.Chrome.ChromeOptions co = new OpenQA.Selenium.Chrome.ChromeOptions();
+            co.AddArgument(@"user-data-dir=c:\Users\Conroe\AppData\Local\Google\Chrome\User Data\");
+            Brouser = new OpenQA.Selenium.Chrome.ChromeDriver(co);
+            Brouser.Navigate().GoToUrl("https://vk.com/audio");
+            
+            IJavaScriptExecutor jse = Brouser as  IJavaScriptExecutor;
+                 jse.ExecuteScript("window.scrollBy(1000,2500000)");
+            Thread.Sleep(10000);
+            if () #stl_side
+            jse.ExecuteScript("window.scrollBy(1000,2500000)");
+            List<IWebElement> VkAudioArtist = Brouser.FindElements(By.ClassName("audio_performer")).ToList();
+            List<IWebElement> VkAudioTrack = Brouser.FindElements(By.ClassName("audio_title")).ToList();
+            List<IWebElement> VkAudioTrackDuration = Brouser.FindElements(By.ClassName("audio_duration")).ToList();
+
+
+
+
+            for (int i = 0; i < VkAudioArtist.Count; i++)
+            {
+                textBox1.AppendText( i + ": "+  VkAudioArtist[i].Text + "-" +  VkAudioTrack[i].Text + " " +VkAudioTrackDuration[i].Text + "\r\n");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
